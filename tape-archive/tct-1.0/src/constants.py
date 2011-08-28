6b50ee5c7c2932f87d48799fc3869e86a53b577f
Sakis Kasampalis <faif@dtek.gr>
2011-08-28 17:54:48 +0300

    final major cleanup

--- tape-archive/tct-1.0/src/constants.py
+++ tape-archive/tct-1.0/src/constants.py
@@ -20,143 +20,85 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-## @package constants
-#  Game's Global Reusable Constants.
-#
-# This module contains some reusable constants
-# which are imported in many places of the game.
+'''Global Reusable Constants.
 
+This module contains some reusable constants
+which are imported in many places of the game.
+'''
+from os import path
 
-# game error return codes
-
-## the module failure exit error signal
+'''error return codes'''
+# the module failure exit error signal
 MOD_FAIL_ERR = 1
 
-## the module import error exit signal
+# the module import error exit signal
 MOD_IMP_ERR = 2
 
-## the file cannot be found, accessed, etc. (IOError)
+# the file cannot be found, accessed, etc. (IOError)
 FILE_ERR = 3
 
-try:
-    from os import path
-# importing os failed, print a custom message...
-except ImportError as err:
-    print((': '.join(("couldn't load module", str(err)))))
-    exit(MOD_IMP_ERR)
-
-# game package info
-
-## the game's version
 GAME_VERSION = '1.0'
-
-## the game's title
 GAME_TITLE = 'the crime tracer, the suspect passer-by'.title()
+GAME_PACKAGE = '{0} (VER-{1})'.format(GAME_TITLE,  GAME_VERSION)
 
-## the game's package
-GAME_PACKAGE = ' '.join((GAME_TITLE, '(V %s).' % (GAME_VERSION)))
-
-# basic directories
-
-## resources directory
+'''basic directories'''
 RESOURCES_DIR = 'resources'
-
-## graphics directory
 GRAPHICS_DIR = 'graphics'
-
-## fonts directory
 FONTS_DIR = 'fonts'
-
-## sounds directory
 SOUNDS_DIR = 'sounds'
-
-## texts directory
 TEXTS_DIR = 'texts'
 
-# sub directories
-
-## introduction directory
+'''sub directories'''
 INTRO_DIR = 'intro'
-
-## menu directory
 MENU_DIR = 'menu'
-
-## share directory
 SHARE_DIR = 'share'
 
-# game resolution
-
-## game's screen width
+'''game resolution'''
 SCREEN_WIDTH = 800
-
-## game's screen height
 SCREEN_HEIGHT = 600
 
-## the scene names
+'''scene names'''
 SCENES = { 'intro':'introduction', 'menu':'main menu', 'level_one': 'first level' }
 
-# game resources' filenames
-
-## the structure of game's resources filenames
+'''game resource filenames'''
 FILES = { 'graphics': {
             'intro': {
-                # the intro slides' filenames
                 'slides': (path.join(INTRO_DIR, 'ct-intro-slide-0.jpg'),
                            path.join(INTRO_DIR, 'ct-intro-slide-1.jpg'),
                            path.join(INTRO_DIR, 'ct-intro-slide-2.jpg')),
-
-                # the intro blank slide filename
                 'blank': [path.join(INTRO_DIR, 'ct-intro-blank-0.jpg')]
             },
             'menu': {
               'main': {
-                  # the main menu's background filenames
                   'bg': [path.join(MENU_DIR, 'ct-menu-main-bg-0.jpg')]
               },
               'settings': {
-                  # the settings menu's background box filenames
                   'box': [path.join(MENU_DIR, 'ct-menu-sets-box-bg-0.png')]
               },
               'share': {
-                  # the window's frame filenames
                   'frame': [path.join(MENU_DIR, 'ct-window-frame-0.png')],
-
-                  # the settings menu's background filenames
                   'bg': [path.join(MENU_DIR, 'ct-menu-sets-bg-0.jpg')],
-
-                  # the mouse cursor filenames
                   'cursor': [path.join(SHARE_DIR, 'ct-menu-mouse-0.png')],
-
-                  # the animation sprites filenames
                   'anim': (path.join(MENU_DIR, 'ct-window-animation-0.png'),
                            path.join(MENU_DIR, 'ct-window-animation-1.png')),
-
-                  # the menu's focus filenames
                   'focus': [path.join(MENU_DIR, 'ct-menu-hover-0.png')]
               }
             },
             'window': {
-                # the icon (in window mode) filenames
                 'icon': [path.join(SHARE_DIR, 'ct-window-icon-0.png')]
             }
           },
           'sounds': {
             'menu': {
               'share': {
-                  # the menu's background music filenames
                   'bg': [path.join(MENU_DIR, 'ct-menu-bg-sound-0.ogg')],
-
-                  # the menu's select filenames
                   'sel': [path.join(MENU_DIR, 'ct-menu-sound-select-0.ogg')],
-
-                  # the menu's focus filenames
                   'focus': [path.join(MENU_DIR, 'ct-menu-sound-focus-0.ogg')]
               }
             }
           },
           'fonts': {
             'menu': {
-                # the menu's custom font filenames
                 'share': ('ct-menu-font-0.ttf',
                           'ct-menu-font-1.ttf',
                           'ct-menu-font-2.ttf',
@@ -166,16 +108,13 @@ FILES = { 'graphics': {
           'texts': {
             'menu': {
               'credits': {
-                  # the main menu's credits text file
                   'text': ['ct-credits-data.txt']
               }
             }
           }
         }
 
-# game information
-
-## the GNU GPL license of the game
+'''other game information'''
 GAME_GPL = (
   "Copyright (C) 2009-11 Free Software Gaming Geeks <fsgamedev@googlegroups.com>",
   " ",
@@ -193,7 +132,6 @@ GAME_GPL = (
   "along with this program.  If not, see <http://www.gnu.org/licenses/>."
 )
 
-## the authors of the game
 GAME_AUTHORS = (
   "Sakis Kasampalis <faif at dtek period gr>",
   "Efstathios Xatzikiriakidis <contact at efxa period gr>",
--- tape-archive/tct-1.0/src/tct.py
+++ tape-archive/tct-1.0/src/tct.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#    Game's Main Entry.
+#    Main Entry Point.
 #
 #    This file is part of The Crime Tracer.
 #
@@ -20,38 +20,29 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+'''Main Entry Point.
 
-## @package tct
-#  Game's Main Entry.
-#
-# This module contains the game's main entry. It checks
-# for any inserted command line options, forms a window
-# and then starts the game manager for the rest things.
-
+This module contains the main entry point. It checks
+for any inserted command line options, creates a window
+and then starts the game manager for the rest things.
+'''
 
 try:
     import constants, os, sys, pygame
     from parse_options import get_parsed_opts
+
     from graphics import load_image
     from game_manager import GameManager
     from base import Base
     from mvc import KeyboardController, EventManager
-except ImportError as err:
-    try:
-        import os
+except Exception as err:
+        import constants, os
         path = os.path.basename(__file__)
-        print((': '.join((path, str(err)))))
-    # importing os failed, print a custom message...
-    except ImportError:
-        print((': '.join(("couldn't load module", str(err)))))
-    exit(2)
+        print('{0}: {1}'.format(path, err))
+        exit(MOD_FAIL_ERR)
 
-
-## objects imported when `from <module> import *' is used
 __all__ = ['main']
 
-## entry point
-#
 def main():
     # change the current directory to the one of the game
     # this is to allow executions like ``python src/tct.py''
@@ -61,7 +52,7 @@ def main():
         print(e)
         exit(constants.FILE_ERR)
 
-    # get game's command line options, return options' flags
+    # get the command line options; return the option flags
     game_opts = get_parsed_opts()
 
     # MVC stuff
@@ -74,7 +65,7 @@ def main():
     # controller which handles the main game loop
     main_controller = MainController(event_manager, gui_view, game_manager)
 
-    # keeping running the game until a quit event occurs
+    # keep running the game until a quit event occurs
     main_controller.run()
 
     # if control somehow reaches this point close all the pygame subsystems
@@ -82,17 +73,9 @@ def main():
     safe_exit()
 
 
-## class of the main controller
-#
-# This class is responsible for running the main game loop until
-# a quit event occurs.
 class MainController:
-    ## create a new controller and register it as a listener
-    #
-    # @param self the object pointer
-    # @param manager the event manager
-    # @param view the view part of MVC
-    # @param gm the game manager
+    '''This class is responsible for running the main game loop until a quit event occurs.'''
+
     def __init__(self, manager, view, gm):
         self.event_manager = manager
         self.event_manager.register_listener(self)
@@ -100,9 +83,6 @@ class MainController:
         self.game_manager = gm
         self.main_loop_running = True
 
-    ## execute the main game loop
-    #
-    # @param self the object pointer
     def run(self):
         while self.main_loop_running:
             # execute the game manager
@@ -117,61 +97,40 @@ class MainController:
             if evt:
                 self.event_manager.post(evt)
 
-    ## handle the related events 
-    #
-    # @param self the object pointer
-    # @param event the generated event
     def notify(self, event):
         if isinstance(event, QuitEvent):
             self.main_loop_running = False
 
-## class of the main GUI view
-#
-# This class is responsible for initialising all the GUI 
-# subsystems and handling the related events
+
 class MainGUIView(Base):
-    ## create a new main GUI view and register it as a listener
-    #
-    # @param self the object pointer
-    # @param manager the event manager
-    # @param game_opts the game's command line arguments
+    '''This class is responsible for initialising all the GUI subsystems and handling the related events.'''
+
     def __init__(self, manager, game_opts):
         self.event_manager = manager
         self.event_manager.register_listener(self)
         self.game_opts = game_opts
 
-        # the game's window title (in window mode)
         GAME_WINDOW_TITLE = constants.GAME_PACKAGE
 
         # make the window of the game always centered
         os.environ["SDL_VIDEO_CENTERED"] = "1"
 
-        # set up the pygame system for the game
         pygame.init()
 
-        # create the window screen
         pygame.display.set_mode(
             (constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT),
             pygame.FULLSCREEN if self.game_opts.fullscreen else 0)
 
-        # set the window title
         pygame.display.set_caption(GAME_WINDOW_TITLE)
 
-        # set the window icon
         pygame.display.set_icon(load_image(
                 constants.FILES['graphics']['window']['icon'][0])[0])
 
-        # hide the mouse cursor
         pygame.mouse.set_visible(False)
 
 
-    ## handle the related events 
-    #
-    # @param self the object pointer
-    # @param event the generated event
     def notify(self, event):
-        pass
+        '''Handle the related events.'''
 
-# run the script if executed
 if __name__ == '__main__':
     main()
