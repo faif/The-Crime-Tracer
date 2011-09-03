536efb6f33b6189e6c3cae833812d71723568968
Sakis Kasampalis <faif@dtek.gr>
2011-09-04 00:34:20 +0300

    cleanup

--- tape-archive/tct-1.0/src/credits.py
+++ tape-archive/tct-1.0/src/credits.py
@@ -20,11 +20,10 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-## @package credits
-#  Game's Credits Screen.
-#
-# This module contains the game's credits screen implementation.
+''' A Credits Screen.
 
+This module contains the menu's credits screen implementation.
+'''
 
 try:
     import constants, pygame
@@ -33,21 +32,16 @@ try:
     from sound_mixer import load_sound, play_sound
     from graphics import load_image, load_font
     from utils import get_time_sec
-except ImportError as err:
-    try:
+except (RuntimeError, ImportError) as err:
         import os
+        from constants import MOD_FAIL_ERR
         path = os.path.basename(__file__)
-        print((': '.join((path, str(err)))))
-    # importing os failed, print a custom message...
-    except ImportError:
-        print((': '.join(("couldn't load module", str(err)))))
-    exit(2)
+        print('{0}: {1}'.format(path, err))
+        exit(MOD_FAIL_ERR)
 
-## objects imported when `from <module> import *' is used
 __all__ = ['Credits']
 
-
-## mapping markup font size token tags with size and color
+# mapping markup font size token tags with size and color
 SIZE_TAGS = {"[XL]":     [60, pygame.Color('white')],
              "[LARGE]":  [50, pygame.Color('cadetblue')],
              "[MEDIUM]": [40, pygame.Color('black')],
@@ -56,70 +50,42 @@ SIZE_TAGS = {"[XL]":     [60, pygame.Color('white')],
 # the next variable value must be synchronized
 # with the first key of the previous structure
 
-## the space between line texts
+# the space between line texts
 LINESPACE = SIZE_TAGS["[XL]"][0]
 
-## mapping markup main authors token tags with real names
+# mapping markup main authors token tags with real names
 AUTH_TAGS = {"[LAFS]": "Efstathios Xatzikiriakidis",
              "[FAIF]": "Sakis Kasampalis"}
 
-## the credits text file comment character 
+# the credits text file comment character 
 CREDITS_COMMENT_TOKEN = "#"
 
-## the default size of a text item
+# the default size of a text item
 FONT_SIZE = 20
 
-## the default color of a text item
+# the default color of a text item
 FONT_COLOR = pygame.Color('black')
 
 SOUND_VOL = 0.2
 SCROLL_SPEED = 50
 SCROLL_FACTOR = 100
 
-
-## class for creating text items
-#
 class TextItem(Base):
 
-    ## initialize the text item
-    #
-    # @param self the object pointer
-    # @param parent the parent's object pointer
-    # @param surface the surface used for blitting
-    # @param text the text itself
-    # @param vertical the vertical offset of the text
-    # @param size the size of the text item
-    # @param color the color of the text item
     def __init__(self, parent, surface, text, vertical,
                  size=FONT_SIZE, color=FONT_COLOR):
-        ## store the parent object
         self.parent = parent
-
-        ## store the surface
         self.surface = surface
-
-        ## store the text
         self.text = text
-
-        ## store the color
         self.color = color
-
-        ## store the vertical offset
         self.vertical = vertical
-
-        ## set the font and size used for the text
         self.font = load_font(
             constants.FILES['fonts']['menu']['share'][3], size)
-
         # render the text with the appropriate attributes
         self.text = self.font.render(self.text, True, self.color)
-
-        ## set the x coordinate (horizontally in the middle)
+        # set the x coordinate (horizontally in the middle)
         self.x = (self.surface.get_width() - self.text.get_width()) / 2.0
 
-    ## draw the text item
-    #
-    # @param self the object pointer
     def redraw(self):
         # set the y coordinate (vertically to the bottom + vertical offset)
         y = self.parent.scroll_root + self.vertical
@@ -127,56 +93,23 @@ class TextItem(Base):
         # blit the text at x, y on the surface given
         self.surface.blit(self.text, (self.x, y))
 
-
-## class for game's credits screen
-#
 class Credits(Base):
 
-    ## initialize the credits screen
-    #
-    # @param self the object pointer
-    # @param screen the screen surface
-    # @param game_opts the game's command line options
-    # @param frame the window frame preloaded image
-    # @param bg the credits background preloaded image
-    # @param sound the preloaded sound on exit
-    # @param textfile the file with the text content
     def __init__(self, screen, game_opts, frame, bg, sound, textfile):
-        ## get the screen surface
         self.screen = screen
-
-        ## the game's command line options
         self.game_opts = game_opts
-
-        ## set the window frame
         self.window_frame = frame
-
-        ## set the credits screen background
         self.credits_bg = bg
-
-        ## set the sound when we return back to the caller
         self.return_sound = sound
-
-        ## flag to control the credits loop
         self.credits_running = None
-
-        ## the list with the rendered text items
         self.text_items = []
-
-        ## set the initial background image x coordinate
         self.bg_x_pos = 0
-
-        ## the total height of the rendered text content
         self.height = 0
-
-        ## the scroll root starts from the bottom of the screen
+        # the scroll root starts from the bottom of the screen
         self.scroll_root = self.screen.get_height()
-
-        ## the speed of the scroll movement
         self.scroll_speed = SCROLL_SPEED
-
-        ## the speed of the background movement it's nice
-        ## for the eye to be the same as the scroll speed
+        # the speed of the background movement; it's nice
+        # for the eye to be the same as the scroll speed
         self.background_speed = self.scroll_speed
 
         with open(textfile) as fin:
@@ -230,16 +163,9 @@ class Credits(Base):
                 # should be the max font size
                 self.height += LINESPACE
 
-        # create clock and track time
         self.clock = pygame.time.Clock()
 
-
-    ## run the loop of the credits screen
-    #
-    # @param self the object pointer
-    # @return inform caller to quit or not
     def run(self):
-        # flag to control the credits loop
         self.credits_running = True
 
         # display & update screen, get all the events
@@ -265,22 +191,20 @@ class Credits(Base):
 
             # handle keyboard keys
             key = pygame.key.get_pressed()
-            # when user presses escape key or 'q'uit key
             if key[K_ESCAPE] or key[K_q]:
                 self.credits_running = False
-
-                # play the return sound
                 if self.game_opts.sound:
                     play_sound(self.return_sound, SOUND_VOL)
 
         # credits screen returns normally
         return True
 
-    ## update the screen objects' position
-    #
-    # @param self the object pointer
-    # @param seconds the time factor to use for moving the objects
     def _update(self, seconds):
+        '''Update the screen objects' position.
+
+        Arguments:
+        seconds -- the time factor to use for moving the objects
+        '''
         # normal scrolling
         if self.scroll_root > self.screen.get_height() - self.height - SCROLL_FACTOR:
             self.scroll_root -= (self.scroll_speed * seconds)
@@ -296,22 +220,15 @@ class Credits(Base):
         if self.bg_x_pos < -self.credits_bg.get_width():
             self.bg_x_pos = 0
 
-    ## redraw the credits screen
-    #
-    # @param self the object pointer
     def _redraw(self):
-
-        # draw the background image
         bg = self.screen.blit(self.credits_bg, (self.bg_x_pos, 0))
 
         # blit the continuation of the background image
         if self.bg_x_pos < - (self.credits_bg.get_width() -
                               self.screen.get_width()):
-
             bg_cont = self.screen.blit(self.credits_bg, (self.bg_x_pos +
                              self.credits_bg.get_width(), 0))
 
-        # draw each text item
         [item.redraw() for item in self.text_items]
 
         # draw the frame of the window
@@ -324,10 +241,3 @@ class Credits(Base):
             need_update = (bg, fr)
 
         return need_update
-
-
-# test the script if executed
-if __name__ == '__main__':
-    import sys
-    print((' '.join(('Testing', sys.argv[0]))))
-    TextItem('parent', 'surface', 'text', 0)
--- tape-archive/tct-1.0/src/intro.py
+++ tape-archive/tct-1.0/src/intro.py
@@ -19,88 +19,56 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+'''Intro Screen.
 
-## @package intro
-#  Intro Screen.
-#
-# This module contains the game's intro screen implementation.
-
+This module contains the game's intro screen implementation.
+'''
 
 try:
     import constants, pygame, sound_mixer, graphics
     from intro_cut_scene import IntroCutScene
     from fsm import State
-except ImportError as err:
-    try:
+except (RuntimeError, ImportError) as err:
         import os
+        from constants import MOD_FAIL_ERR
         path = os.path.basename(__file__)
-        print((': '.join((path, str(err)))))
-    # importing os failed, print a custom message...
-    except ImportError:
-        print((': '.join(("couldn't load module", str(err)))))
-    exit(2)
+        print('{0}: {1}'.format(path, err))
+        exit(MOD_FAIL_ERR)
 
-## objects imported when `from <module> import *' is used
 __all__ = ['Intro']
 
 MAX_VOLUME = 1.0
 
-
-## class for game's intro screen
-#
 class Intro(State):
 
-    ## initialize the intro screen
-    #
-    # @param self the object pointer
-    # @param game_opts the game's command line options
     def __init__(self, game_opts):
-        # initialize the state
         State.__init__(self, constants.SCENES['intro'])
-
-        ## the game's command line options
         self.game_opts = game_opts
 
-        ## intro slides
+        # intro slides
         slide_num = len(constants.FILES['graphics']['intro']['slides'])
         self.slides = [
             graphics.load_image(
                 constants.FILES['graphics']['intro']
                 ['slides'][i])[0] for i in range(slide_num)
             ]
-
-        ## cut scenes object
         self.cutscenes = IntroCutScene(self.slides)
 
-        # set sound volume to minimum
         pygame.mixer.music.set_volume(0.0)
-
-        # play the background music theme
         sound_mixer.play_music(
             constants.FILES['sounds']['menu']['share']['bg'][0])
-
-        # pause or unpause music according to user preference
         if self.game_opts.music:
             pygame.mixer.music.unpause()
         else:
             pygame.mixer.music.pause()
-
-        # set sound volume to maximum
         pygame.mixer.music.set_volume(MAX_VOLUME)
 
-    ## what to do when the intro is enabled
-    #
-    # @param self the object pointer
     def do_actions(self):
         # run the intro slideshow
         self.cutscenes.run()
 
-    ## what should be satisfied for enabling the next scene
-    #
-    # @param self the object pointer        
-    # @return the name of the next scene        
     def check_conditions(self):
-        # if the scene is finished, returns back
+        # if the scene is finished, return back
         # to the caller the name of the next one
         if self.cutscenes.controller.is_finished:
             return constants.SCENES['menu']
